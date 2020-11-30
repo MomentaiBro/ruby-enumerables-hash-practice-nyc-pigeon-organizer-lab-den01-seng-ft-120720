3fbda2c5eb4f2caae3b7df16f7a9bfabf6940977
Alexander S <alx.sherwin@gmail.com>
2020-11-30 01:49:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,11 +8,10 @@ def nyc_pigeon_organizer(data)
           final_array = {}
         end
         if !final_array[name][key]
-          final_array[name][key]=[]
+          !final_array[name][key]=[]
         end
         final_array[name][key].push(inner_key)
       end
     end
   end
-  final_results
 end
